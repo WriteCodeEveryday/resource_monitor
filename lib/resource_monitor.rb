5ef2bad13190a91e759146587897a9166aa6ff19
Lazaro Herrera <lazherrera@gmail.com>
2017-04-08 19:13:30 -0400

    Some fixes in html and ruby code

--- lib/resource_monitor.rb
+++ lib/resource_monitor.rb
@@ -10,7 +10,7 @@ module ResourceMonitor
       cpu_percentage: cpu_per,
       ram_percentage: mem_per,
       ram_usage: (size / 1024).to_i,
-      last_executed_line: caller[1] || "NoCallerData", # Skip one so this is not triggered on Application.before_action
+      last_executed_line: caller[2] || "NoCallerData", # Skip two so this is not triggered on Application.before_action
       last_executed_controller: controller&.controller_name || 'NoController',
       last_executed_action: controller&.action_name || 'NoAction'
     }

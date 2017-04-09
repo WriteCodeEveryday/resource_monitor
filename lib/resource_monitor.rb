b09f4a6a839162cfa438412c6d1189cbdf07c719
Lazaro Herrera <lazherrera@gmail.com>
2017-04-08 22:59:55 -0400

    Fixed issues with ActionCable

--- lib/resource_monitor.rb
+++ lib/resource_monitor.rb
@@ -6,17 +6,25 @@ module ResourceMonitor
     # Recover the process id and memory usage in KB (http://stackoverflow.com/questions/7220896/get-current-ruby-process-memory-usage)
     pid, size, cpu_per, mem_per = `ps ax -o pid,rss,%cpu,%mem | grep -E "^[[:space:]]*#{$$}"`.strip.split.map(&:to_i)
 
+    if defined?(Rails)
+      last_executed_controller = controller&.controller_name.presence  || controller&['controller_name'].presence  || 'NoController'
+      last_executed_action = controller&.action_name.presence || controller&['action_name'].presence || 'NoAction'
+    else
+      last_executed_controller = controller&.controller_name ||  'NoController'
+      last_executed_action = controller&.action_name || 'NoAction'
+    end
+    
     data = {
       process_id: pid,
       cpu_percentage: cpu_per,
       ram_percentage: mem_per,
       ram_usage: (size / 1024).to_i,
-      last_executed_line: caller[2] || "NoCallerData", # Skip two so this is not triggered on Application.before_action
-      last_executed_controller: controller&.controller_name || 'NoController',
-      last_executed_action: controller&.action_name || 'NoAction'
+      last_executed_line: caller[2]|| "NoCallerData", # Skip two so this is not triggered on Application.before_action
+      last_executed_controller: last_executed_controller,
+      last_executed_action: last_executed_action
     }
 
-    ActionCable.server.broadcast 'resources', data if defined?(ActionCable)
+    ActionCable.server.broadcast 'resources', data.to_json if defined?(ActionCable)
     return data
   end
 end
--- lib/tasks/rm_console.rake
+++ lib/tasks/rm_console.rake
@@ -1,4 +1,5 @@
 namespace :resource_monitor do
+  # Works perfectly as a quick test environment.
   task :console do
     require 'irb'
     require 'irb/completion'
@@ -7,18 +8,20 @@ namespace :resource_monitor do
     IRB.start
   end
 
+  # Works correctly.
   task :install_dashboard do
     if defined?(Rails)
       path = Gem.loaded_specs['resource_monitor'].full_gem_path
       puts "Copying from #{path}/app/ to #{Rails.root.to_s}/app/"
       FileUtils.cp_r "#{path}/app/.", "#{Rails.root.to_s}/app/"
       puts "IMPORTANT: Remember to add the following route to your config/routes.rb"
-      puts "get 'resources' => 'resource#index'"
+      puts "get '/resources/' => 'resource#index'"
     else
       puts "This installer should be run inside a Rails project."
     end
   end
 
+  # Does nothing at all.
   task :uninstall_dashboard do
     if defined?(Rails)
       path = Gem.loaded_specs['resource_monitor'].full_gem_path
@@ -28,7 +31,7 @@ namespace :resource_monitor do
         FileUtils.rm "#{Rails.root.to_s}#{item.split(path)[1]}"
       end
       puts "IMPORTANT: Remember to remove the following route from your config/routes.rb"
-      puts "get 'resources' => 'resource#index'"
+      puts "get '/resources/' => 'resource#index'"
     else
       puts "This uninstaller should be run inside a Rails project."
     end

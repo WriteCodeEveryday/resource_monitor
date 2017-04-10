8207448ed3641c2672db4d777ec759fee87ec6ee
Lazaro Herrera <lazherrera@gmail.com>
2017-04-10 02:25:43 -0400

    allow ActiveRecord and free-form details

--- lib/resource_monitor.rb
+++ lib/resource_monitor.rb
@@ -2,16 +2,18 @@ require "resource_monitor/engine"
 require "resource_monitor/version"
 
 module ResourceMonitor
-  def self.benchmark(controller=nil)
+  def self.benchmark(input=nil)
     # Recover the process id and memory usage in KB (http://stackoverflow.com/questions/7220896/get-current-ruby-process-memory-usage)
     pid, size, cpu_per, mem_per = `ps ax -o pid,rss,%cpu,%mem | grep -E "^[[:space:]]*#{$$}"`.strip.split.map(&:to_i)
 
-    if controller
-      last_executed_controller = controller.controller_name  || 'NoController'
-      last_executed_action = controller.action_name || 'NoAction'
+    if input
+      if input.instance_of(ActionController)
+        details = "Controller: #{input.controller_name}\nAction: #{input.action_name}"
+      elsif input.instance_if(ActiveRecord)
+        details = "Model: #{input.model_name.human}"
+      end
     else
-      last_executed_controller = 'NoController'
-      last_executed_action = 'NoAction'
+      details = "No details available"
     end
 
     data = {
@@ -19,8 +21,7 @@ module ResourceMonitor
       cpu_percentage: cpu_per,
       ram_percentage: mem_per,
       ram_usage: (size / 1024).to_i,
-      last_executed_controller: last_executed_controller,
-      last_executed_action: last_executed_action
+      details: details
     }
 
     ActionCable.server.broadcast 'resources', data.to_json if defined?(ActionCable)
